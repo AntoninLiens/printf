17bc5710dec09801772dd55816aacc3e9fb0c978
Antonin Liens <aliens@mi-r3-p4.s19.be>
2021-01-21 13:52:44 +0100

    fix type xX

--- ft_type_x.c
+++ ft_type_x.c
@@ -6,7 +6,7 @@
 /*   By: aliens <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 15:18:03 by aliens            #+#    #+#             */
-/*   Updated: 2021/01/21 12:46:20 by aliens           ###   ########.fr       */
+/*   Updated: 2021/01/21 13:52:15 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,45 +19,50 @@ static int	ft_size(unsigned int n)
 	return (1 + ft_size(n / 16));
 }
 
-int			ft_flags_before(int size)
+int			ft_flags_before(int size, int i)
 {
-	int	i;
-
-	i = 0;
 	if (list.flags[0] == 2)
-		while (++i < list.prec[0] - size)
-			write(1, " ", 1);
-	else if (list.flags[0] == 4)
-		while (++i < list.prec[0] - size)
+		while (i++ < list.prec[0] - size)
+		{
+			if (list.flags[1])
+				write(1, " ", 1);
+			else
+				write(1, "0", 1);
+		}
+	else if (list.flags[0] == 3)
+		while (i++ < list.prec[0] - size)
 			write(1, "0", 1);
+	else if (list.flags[0] == 4)
+		while (i++ < list.prec[0] - size)
+			write(1, " ", 1);
 	return (i);
 }
 
 int			ft_type_x_dot(unsigned int n, char *base, char *res)
 {
-	int		u_size;
 	int		size;
+	int		x_i;
 	int		i;
 	int		j;
 
-	i = -1;
-	j = -1;
+	i = 0;
+	j = 0;
 	size = list.prec[1] < ft_size(n) ? ft_size(n) : list.prec[1];
-	i += ft_flags_before(size);
-	while (++j < size - ft_size(n))
+	x_i = size;
+	i = ft_flags_before(size, i);
+	while (j++ < size - x_i)
 		write(1, "0", 1);
-	u_size = ft_size(n);
-	while (u_size--)
+	while (x_i--)
 	{
-		res[u_size] = base[n % 16];
+		res[x_i] = base[n % 16];
 		n /= 16;
 	}
 	ft_putstr_fd(res, 1);
 	free(res);
 	if (list.flags[0] == 1)
-		while (++i < list.prec[0] - size)
+		while (i++ < list.prec[0] - size)
 			write(1, " ", 1);
-	return (size + i);
+	return (size + i - 1);
 }
 
 int			ft_type_x(unsigned int n, char *base)
@@ -65,23 +70,25 @@ int			ft_type_x(unsigned int n, char *base)
 	char	*res;
 	int		size;
 	int		i;
+	int		x_i;
 
 	i = 0;
 	size = ft_size(n);
+	x_i = size;
 	if (!(res = ft_calloc(sizeof(char), size + 1)))
 		return (0);
 	if (list.flags[1])
 		return (ft_type_x_dot(n, base, res));
-	i += ft_flags_before(size);
-	while (size--)
+	i = ft_flags_before(size, i);
+	while (x_i--)
 	{
-		res[size] = base[n % 16];
+		res[x_i] = base[n % 16];
 		n /= 16;
 	}
 	ft_putstr_fd(res, 1);
 	free(res);
 	if (list.flags[0] == 1)
-		while (i++ < list.prec[0] - ft_size(n))
+		while (i++ < list.prec[0] - size)
 			write(1, " ", 1);
-	return (i ? ft_size(n) + i - 1 : ft_size(n) + 1);
+	return (i ? ft_size(n) + i : ft_size(n) + 1);
 }
