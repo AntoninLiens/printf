d0257373374f625e9fe3f6afa8fd33d11008a21e
Aliens <aliens@MBP-Julie.fritz.box>
2021-01-28 14:53:05 +0100

    type p fix

--- ft_type_p.c
+++ ft_type_p.c
@@ -6,7 +6,7 @@
 /*   By: aliens <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:44:06 by aliens            #+#    #+#             */
-/*   Updated: 2021/01/21 18:23:52 by aliens           ###   ########.fr       */
+/*   Updated: 2021/01/28 14:49:22 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int			ft_type_p_dot(char *dst, char *hex, unsigned long ptr2)
 {
 	int i;
 	int j;
+	int dot;
 	int p_i;
 	int size;
 
@@ -23,8 +24,9 @@ int			ft_type_p_dot(char *dst, char *hex, unsigned long ptr2)
 	j = -1;
 	size = list.prec[1] < ft_hex_size(ptr2) ? ft_hex_size(ptr2) : list.prec[1];
 	p_i = ft_hex_size(ptr2);
+	dot = !ptr2 && !list.prec[1] ? 1 : 0;
 	if (list.flags[0] == 4 || list.flags[0] == 2)
-		while (++i < list.prec[0] - size - 2)
+		while (-dot + ++i < list.prec[0] - size - 2)
 			write(1, " ", 1);
 	ft_putstr("0x");
 	while (++j < list.prec[1] - p_i)
@@ -34,7 +36,7 @@ int			ft_type_p_dot(char *dst, char *hex, unsigned long ptr2)
 		dst[p_i] = hex[ptr2 % 16];
 		ptr2 /= 16;
 	}
-	ft_putstr(dst);
+	dot ? size-- : ft_putstr(dst);
 	free(dst);
 	if (list.flags[0] == 1)
 		while (++i < list.prec[0] - size - 2)
@@ -49,6 +51,7 @@ int			ft_type_p(void *ptr, char *hex)
 	int				size;
 	int				i;
 	int				p_i;
+	int				dot;
 
 	i = 0;
 	ptr2 = (unsigned long)ptr;
@@ -58,22 +61,23 @@ int			ft_type_p(void *ptr, char *hex)
 		return (0);
 	if (list.flags[1])
 		return (ft_type_p_dot(dst, hex, ptr2));
+	dot = list.flags[0] == 3 && !(unsigned long)ptr && !list.prec[0] ? 1 : 0;
 	if (list.flags[0] == 4)
-		while (i++ < list.prec[0] - size - 2)
+		while (i++ < list.prec[0] - size - 2 - dot)
 			write(1, " ", 1);
 	ft_putstr("0x");
 	if (list.flags[0] == 2)
-		while (i++ < list.prec[0] - size - 2)
+		while (i++ < list.prec[0] - size - 2 - dot)
 			write(1, "0", 1);
 	else if (list.flags[0] == 3)
-		while (i++ < list.prec[0] - size)
+		while (i++ < list.prec[0] - size - dot)
 			write(1, "0", 1);
 	while (p_i--)
 	{
 		dst[p_i] = hex[ptr2 % 16];
 		ptr2 /= 16;
 	}
-	ft_putstr(dst);
+	dot ? size-- : ft_putstr(dst);
 	free(dst);
 	if (list.flags[0] == 1)
 		while (i++ < list.prec[0] - size - 2)
